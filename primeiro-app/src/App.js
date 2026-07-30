f14718950b28c01890f8470667a1fa6ccf8640be
Gabriele Natividade <natividadegabriele438@gmail.com>
2026-07-29 21:21:37 -0300

    refactor: clean up project structure by removing unused files and updating index.html

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          primeiro app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bem-Vindo ao meu Projeto</h1>  
     </div>
   );
 }
