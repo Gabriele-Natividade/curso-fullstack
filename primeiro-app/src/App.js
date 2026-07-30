71ca009146731abae78c2e6293f483234cd5480f
Gabriele Natividade <natividadegabriele438@gmail.com>
2026-07-30 16:18:06 -0300

    feat: update App component to manage student name state and handle name change

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,19 +1,26 @@
+import { useState } from "react";
+
+
 import Nome from "./components/Nome";
 
-function App(){
+function App() {
+  const [aluno, setAluno] = useState("Sujeito Programador");
+
+  function hanleChangeName() {
+    setAluno("Gabriele");
+  }
+
   return (
     <div>
-      <h1>Componente App</h1>
-      <Nome aluno="João" idade={20}/>
-      <br/> 
-      <Nome aluno="Maria" idade={25}/>
-    </div>  
+      <h1>Componente App</h1><br />
+      <h2>Olá: {aluno}</h2>
+      <button onClick={hanleChangeName}>
+        Mudar Nome
+      </button>
+    </div>
   );
 }
 
-
-
 export default App;
-
 
 
