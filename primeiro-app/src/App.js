b3b22c322ee2bcc5f21b3000e8d265440db076ea
Gabriele Natividade <natividadegabriele438@gmail.com>
2026-08-04 14:30:08 -0300

    feat: add useEffect hook to monitor tarefas state changes

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [input, setInput] = useState("");
@@ -6,6 +6,9 @@ function App() {
     "Pagar a conta de luz",
     "Estudar React",
   ]);
+
+  useEffect(() => {}, [tarefas]);
+
   function handRegister(e) {
     e.preventDefault();
 
