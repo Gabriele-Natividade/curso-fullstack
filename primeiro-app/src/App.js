07b6ab390c305c92134f47a031391590745e9cdc
Gabriele Natividade <natividadegabriele438@gmail.com>
2026-08-04 14:49:00 -0300

    feat: add useEffect to initialize tarefas state from localStorage

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -8,6 +8,13 @@ function App() {
   ]);
 
   useEffect(() => {
+    const tarefasStorage = localStorage.getItem("@tarefas");
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
     localStorage.setItem("@tarefas", JSON.stringify(tarefas));
   }, [tarefas]);
 
